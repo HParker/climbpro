d3c3618cd2a7c158b4453893965b02c99723ae79
Adam Hess <adamhess1991@gmail.com>
2017-09-28 17:33:05 -0500

    Catch dups from different pieces
    There was a class of duplicate board states where the board looks identical,
    but the pieces are technically different. We can safely ignore these.
    This allows us to finish climbpro 12 in about 40s

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 use std::time::{Instant};
 use std::collections::HashSet;
 use std::hash::{Hash,Hasher};
+use std::collections::hash_map::DefaultHasher;
 
 struct Shape {
     collider: [u8; 3],
@@ -15,6 +16,7 @@ struct Shape {
 struct Piece {
     origin: (usize, usize),
     shape: &'static Shape,
+    id: u8,
     movable: bool
 }
 
@@ -25,24 +27,29 @@ struct Board {
     width: usize
 }
 
+impl Board {
+    fn to_hash(&self) -> u64 {
+        let mut hash = DefaultHasher::new();
+        self.hash(&mut hash);
+        hash.finish()
+    }
+}
+
 impl Hash for Board {
     fn hash<H: Hasher>(&self, state: &mut H) {
-        for (i, piece) in self.pieces.iter().enumerate() {
+        let mut sorted_pieces = self.pieces.clone();
+        sorted_pieces.sort_by(|me, other| me.origin.cmp(&other.origin));
+        for piece in  sorted_pieces.iter() {
             piece.origin.0.hash(state);
             piece.origin.1.hash(state);
-            i.hash(state);
+            piece.id.hash(state);
         }
     }
 }
 
 impl PartialEq for Board {
     fn eq(&self, other: &Board) -> bool {
-        for (piece, other_piece) in self.pieces.iter().zip(other.pieces.iter()) {
-            if piece.origin != other_piece.origin {
-                return false;
-            }
-        }
-        true
+        self.to_hash() ==  other.to_hash()
     }
 }
 impl Eq for Board {}
@@ -50,18 +57,18 @@ impl Eq for Board {}
 
 static DOT: Shape = Shape {
     collider: [0b100, 0b000, 0b000],
-    // collider: [[X, O, O],
-    //            [O, O, O],
-    //            [O, O, O]],
+    // X O O
+    // O O O
+    // O O O
     height: 1,
     width: 1
 };
 
 static VTWO: Shape = Shape {
     collider: [0b100, 0b100, 0b000],
-    // collider: [[X, O, O],
-    //            [X, O, O],
-    //            [O, O, O]],
+    // X O O
+    // X O O
+    // O O O
     height: 2,
     width: 1
 };
@@ -128,59 +135,61 @@ fn show(pieces: &[Piece], height: usize, width: usize) {
 }
 
 fn ten_board() -> Board {
-    Board { pieces: vec!(
-        Piece { origin: (1,0), shape: &LEFT,   movable: true },
-        Piece { origin: (2,2), shape: &DOT,    movable: true },
-        Piece { origin: (3,1), shape: &SQUARE, movable: true },
-        Piece { origin: (1,3), shape: &VTWO,   movable: true },
-        Piece { origin: (3,0), shape: &DOT,    movable: true },
-        Piece { origin: (3,3), shape: &DOT,    movable: true },
-        Piece { origin: (4,0), shape: &VTWO,   movable: true },
-        Piece { origin: (5,1), shape: &DOT,    movable: true },
-        Piece { origin: (4,2), shape: &RIGHT,  movable: true },
-        Piece { origin: (0,0), shape: &DOT,    movable: false },
-        Piece { origin: (0,3), shape: &DOT,    movable: false },
-    ),
-            height: 6,
-            width: 4,
+    Board {
+        pieces: vec!(
+            Piece { origin: (1,0), shape: &LEFT,   id: 1, movable: true },
+            Piece { origin: (2,2), shape: &DOT,    id: 2, movable: true },
+            Piece { origin: (3,1), shape: &SQUARE, id: 3, movable: true },
+            Piece { origin: (1,3), shape: &VTWO,   id: 4, movable: true },
+            Piece { origin: (3,0), shape: &DOT,    id: 2, movable: true },
+            Piece { origin: (3,3), shape: &DOT,    id: 2, movable: true },
+            Piece { origin: (4,0), shape: &VTWO,   id: 4, movable: true },
+            Piece { origin: (5,1), shape: &DOT,    id: 2, movable: true },
+            Piece { origin: (4,2), shape: &RIGHT,  id: 5, movable: true },
+            Piece { origin: (0,0), shape: &DOT,    id: 2, movable: false },
+            Piece { origin: (0,3), shape: &DOT,    id: 2, movable: false },
+        ),
+        height: 6,
+        width: 4,
     }
 }
 
 
 fn twelve_board() -> Board {
-    Board { pieces: vec!(
-        Piece { origin: (1,0), shape: &VTWO,  movable: true },
-        Piece { origin: (1,4), shape: &VTWO,  movable: true },
-        Piece { origin: (4,1), shape: &GOAL,  movable: true },
-        Piece { origin: (2,1), shape: &LEFT,  movable: true },
-        Piece { origin: (2,2), shape: &RIGHT, movable: true },
-        Piece { origin: (3,0), shape: &DOT,   movable: true },
-        Piece { origin: (3,4), shape: &DOT,   movable: true },
-        Piece { origin: (5,0), shape: &DOT,   movable: true },
-        Piece { origin: (5,4), shape: &DOT,   movable: true },
-        Piece { origin: (4,0), shape: &FTWO,  movable: true },
-        Piece { origin: (4,3), shape: &FTWO,  movable: true },
-        Piece { origin: (0,0), shape: &FTWO,  movable: false },
-        Piece { origin: (0,3), shape: &FTWO,  movable: false },
-    ),
-            height: 6,
-            width: 5
+    Board {
+        pieces: vec!(
+            Piece { origin: (1,0), shape: &VTWO,  id: 4, movable: true },
+            Piece { origin: (1,4), shape: &VTWO,  id: 4, movable: true },
+            Piece { origin: (4,1), shape: &GOAL,  id: 6, movable: true },
+            Piece { origin: (2,1), shape: &LEFT,  id: 1, movable: true },
+            Piece { origin: (2,2), shape: &RIGHT, id: 5, movable: true },
+            Piece { origin: (3,0), shape: &DOT,   id: 2, movable: true },
+            Piece { origin: (3,4), shape: &DOT,   id: 2, movable: true },
+            Piece { origin: (5,0), shape: &DOT,   id: 2, movable: true },
+            Piece { origin: (5,4), shape: &DOT,   id: 2, movable: true },
+            Piece { origin: (4,0), shape: &FTWO,  id: 7, movable: true },
+            Piece { origin: (4,3), shape: &FTWO,  id: 7, movable: true },
+            Piece { origin: (0,0), shape: &FTWO,  id: 7, movable: false },
+            Piece { origin: (0,3), shape: &FTWO,  id: 7, movable: false },
+        ),
+        height: 6,
+        width: 5
     }
 }
 
 fn movements(piece: &Piece, height: usize, width: usize) -> Vec<Piece> {
     let mut new_pieces = vec!();
     if piece.origin.0 + piece.shape.height < height {
-        new_pieces.push(Piece { origin: (piece.origin.0 + 1, piece.origin.1), shape: piece.shape, movable: true })
+        new_pieces.push(Piece { origin: (piece.origin.0 + 1, piece.origin.1), id: piece.id, shape: piece.shape, movable: true })
    }
     if piece.origin.1 + piece.shape.width < width {
-        new_pieces.push(Piece { origin: (piece.origin.0, piece.origin.1 + 1), shape: piece.shape, movable: true })
+        new_pieces.push(Piece { origin: (piece.origin.0, piece.origin.1 + 1), id: piece.id, shape: piece.shape, movable: true })
     }
     if piece.origin.0 > 0 {
-        new_pieces.push(Piece { origin: (piece.origin.0 - 1, piece.origin.1), shape: piece.shape, movable: true })
+        new_pieces.push(Piece { origin: (piece.origin.0 - 1, piece.origin.1), id: piece.id, shape: piece.shape, movable: true })
     }
     if piece.origin.1 > 0 {
-        new_pieces.push(Piece { origin: (piece.origin.0, piece.origin.1 - 1), shape: piece.shape, movable: true })
+        new_pieces.push(Piece { origin: (piece.origin.0, piece.origin.1 - 1), id: piece.id, shape: piece.shape, movable: true })
     }
     new_pieces
 }
@@ -207,7 +216,7 @@ fn area_for(pieces: &[Piece], ignore_location: usize, height: usize, width: usiz
 fn valid(piece: &Piece, area: &[u8], _height: usize, width: usize) -> bool {
     for (y, row) in piece.shape.collider.iter().enumerate() {
         if row != &0b0 && ((area[piece.origin.0 + y] & (row << (width - 1) - piece.origin.1)) != 0b0) {
-            return false;
+            return false
         }
     }
     true
@@ -215,16 +224,19 @@ fn valid(piece: &Piece, area: &[u8], _height: usize, width: usize) -> bool {
 
 fn potential_boards(board: &Board, seen_boards: &mut HashSet<Board>) -> Vec<Board> {
     let mut potentials: Vec<Board> = vec!();
+
     for (i, piece) in board.pieces.iter().enumerate().filter(|p| p.1.movable) {
         let area: [u8; 6] = area_for(&board.pieces, i, board.height, board.width);
+
         for moved_piece in movements(piece, board.height, board.width) {
             if valid(&moved_piece, &area, board.height, board.width) {
                 let mut new_board: Board = board.clone();
                 replace(moved_piece, i, &mut new_board);
-                if !seen_boards.contains(&new_board) {
-                    seen_boards.insert(new_board.clone());
+                let new = seen_boards.insert(new_board.clone());
+                if new {
                     potentials.push(new_board);
                 }
+
             }
         }
     }
@@ -243,13 +255,12 @@ fn goal(board: &Board) -> bool {
     board.pieces[2].origin.0 == 0
 }
 
-
 fn main() {
     let now = Instant::now();
-    let mut layer: Vec<Board> = vec!(ten_board());
+    let mut layer: Vec<Board> = vec!(twelve_board());
     let mut seen_boards: HashSet<Board> = HashSet::new();
     let mut counter: usize = 0;
-    while counter < 90 {
+    while counter < 200 {
         layer = expand_layer(&layer, &mut seen_boards);
         match layer.iter().find(|b| goal(b)) {
             Some(b) => {
